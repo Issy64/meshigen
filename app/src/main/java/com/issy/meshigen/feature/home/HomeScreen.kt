1d9147ac786d64ff1ab2e6d6ed7a9c8ccc804059
Issy64 <isikoron64@gmail.com>
2026-04-06 23:21:41 +0900

    feat: HomeScreenの画面骨組みを追加
    - Scaffold と Column を使った HomeScreen の土台を追加
    今後の入力UIや結果表示を載せるための基本レイアウトを用意

---
+++ app/src/main/java/com/issy/meshigen/feature/home/HomeScreen.kt
@@ -0,0 +1,25 @@
+package com.issy.meshigen.feature.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+
+        }
+    }
+}
