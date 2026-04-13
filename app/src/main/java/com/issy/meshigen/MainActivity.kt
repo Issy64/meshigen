62c9685ccacad660856d1beca1e521c6cb0bead9
Issy64 <isikoron64@gmail.com>
2026-04-13 17:04:57 +0900

    feat: gourmetsの初回シード投入を実装
    - 30件のGourmetSeedDataを追加
    - DB起動時に件数チェックして初回のみinsertAllを実行
    - アプリ起動時にDB初期化を行うようMainActivityを更新

--- app/src/main/java/com/issy/meshigen/MainActivity.kt
+++ app/src/main/java/com/issy/meshigen/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
+import com.issy.meshigen.data.local.DatabaseProvider
 import com.issy.meshigen.navigation.MeshigenAppShell
 import com.issy.meshigen.ui.theme.MeshigenTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            DatabaseProvider.get(applicationContext)
+        }
+
         setContent {
             MeshigenTheme {
                 MeshigenAppShell()
